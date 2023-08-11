5f074ca5e45358ae4b4f2f78f442386b692fc8c5
mariferraldo <mariferraldo@gmail.com>
2023-08-10 20:42:41 -0400

    workshop 18

---
+++ workshop18/index.js
@@ -0,0 +1,53 @@
+//Unit Tests
+// A function called "multiplication" that returns the product of the two input numbers.
+/* declare:multiply 
+what to expect (x, y)
+show a product = x * y
+return the product 
+
+unexpected
+put other than numbers 
+multiply numbers and strings 
+input the incorrect sign 
+
+*/
+
+//A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+/* declare function: concatOdds
+identify the odds numbers 
+combine in array 
+return the sorted array 
+
+unexpected
+#put not numbers, strings, non intergers, words
+#if there are dupliclate inputs 
+*/
+
+//Functional Tests
+//A shopping cart checkout feature that allows a user to check out as a guest (without an account), or as a logged-in user. 
+/*
+propmt to logged in checkout 
+prompt the user to login 
+prompt to use the name on file to be delivery the order 
+ask which address they would like to delivery  if it is the one they have saved or a new one 
+used save or new info of a payment method 
+review card
+confirm order
+
+guest checkout
+proceed to input personal info 
+proceed to input address to be delivery 
+input thepayment information 
+review card 
+confirm order 
+
+unexpected
+if the card is empty it should alert and not let the client go foward t checkout 
+missing behaviors 
+if payment fails 
+wrong address 
+out of stock order 
+sizes unavailible 
+*/
+
+
